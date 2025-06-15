1e32597a9f8df6cc0763ecda0337d3b7330bd95b
SaridDev <saridmf.dev@gmail.com>
2025-06-15 17:49:57 -0600

    menu-toggle responsive

--- Scripts/landing_animations.js
+++ Scripts/landing_animations.js
@@ -1,3 +1,25 @@
+/*Menu colapsado-responsive */
+const menuBtn = document.querySelector(".menu-button")
+const closeBtn = document.querySelector('.close-button')
+const header = document.querySelector(".header")
+const links = document.querySelectorAll(".nav-group a")
+
+menuBtn.onclick = function(){
+    header.classList.add('active')
+}
+
+closeBtn.onclick = function(){
+    header.classList.remove('active')
+}
+
+links.forEach((link)=>{
+    link.onclick = function(){
+        header.classList.remove('active')
+    }
+})
+
+
+
 //animacion de scroll
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
