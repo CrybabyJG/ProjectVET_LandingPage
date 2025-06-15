0c83991371bff5fb7f21fa12f5304e1330492eb6
SaridDev <saridmf.dev@gmail.com>
2025-06-15 16:01:17 -0600

    Animacion de scroll y  btnScroll

---
+++ Scripts/landing_animations.js
@@ -0,0 +1,33 @@
+//animacion de scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+});
+
+document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
+
+
+
+//animacion de scroll btn
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
